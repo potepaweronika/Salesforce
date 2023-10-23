2024e3be3af25fe7ab2d8008a50ceb2e2bc95aba
Weronika Potępa <s95534@pollub.edu.pl>
2023-10-23 21:54:18 +0200

    Fixed LWC

--- force-app/main/default/lwc/calculateTax/calculateTax.js
+++ force-app/main/default/lwc/calculateTax/calculateTax.js
@@ -1,26 +1,43 @@
-import { LightningElement, wire, track } from 'lwc';
+import { LightningElement, wire, track, api } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
-import INVOICE_OBJECT from '@salesforce/schema/Invoice__c';
-import calculateTax from '@salesforce/apex/InvoiceCalculator.calculateTax';
+// import INVOICE_OBJECT from '@salesforce/schema/Invoice__c';
+import calculateTaxForCountry from '@salesforce/apex/InvoiceCalculator.calculateTaxForCountry';
 import getCountryOptions from '@salesforce/apex/CountryController.getCountryOptions';
 
-export default class CalculateTax extends LightningElement {
-    @track selectedCountry = '';
-    @track calculatedTax = 0;
-    @track countryOptions = [];
-    @track invoiceId; // Store the selected invoice record Id
+const FIELDS = ["Invoice__c.Amount__c", "Invoice__c.Country__c"];
 
-    @wire(getRecord, { recordId: '$invoiceId', fields: [INVOICE_OBJECT.Country__c, INVOICE_OBJECT.Amount__c] })
-    invoice;
+export default class CalculateTax extends LightningElement {
+    @api recordId;
+    @track selectedCountry = '';
+    @track tax = 0;
+    @track calculatedTax = 0;
+    @track countryOptionsProxy = [];
+    countryOptions = ["test1", "test2"];
+
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+    wiredInvoice({ error, data }) {
+        if (data) {
+            this.invoice = data;
+            this.calculateTax(); // Recalculate tax when the invoice data changes
+        } else if (error) {
+            console.error('Error fetching invoice:', error);
+        }
+    }
+
 
     // Load country options from the server
     @wire(getCountryOptions)
     wiredCountryOptions({ data, error }) {
         if (data) {
-            this.countryOptions = data;
+            this.countryOptionsProxy = data;
+            this.countryOptions = JSON.parse(JSON.stringify(this.countryOptionsProxy));
         } else if (error) {
             return 0;
         }
+    }
+
+    get options() {
+        return this.countryOptions.map(country => ({ label: country, value: country }));;
     }
 
     // Handle country selection
@@ -31,50 +48,18 @@ export default class CalculateTax extends LightningElement {
 
     // Calculate tax based on the selected country
     calculateTax() {
-        if (this.selectedCountry && this.invoice.data) {
-            calculateTax({ invoice: this.invoice.data, selectedCountry: this.selectedCountry })
-                .then(result => {
-                    this.calculatedTax = result;
-                })
-                .catch(error => {
-                    // Handle any errors from the server call
-                    console.error('Error calculating tax: ' + JSON.stringify(error));
-                });
+        if (this.selectedCountry && this.recordId) {
+            // Calculate tax
+            this.tax = calculateTaxForCountry({ amount: this.invoice.fields.Amount__c.value, country: this.selectedCountry })
+              .then(value => {
+                this.calculatedTax = value;
+              })
+              .catch((err) => {
+                console.error(err);
+              });
         } else {
             // Handle the case where no country is selected or invoice data is not available
+            console.log('No country selected or invoice data not available');
         }
     }
 }
-
-
-// import { LightningElement, wire, track } from 'lwc';
-// import getCountryOptions from '@salesforce/apex/CountryController.getCountryOptions';
-// import getTaxRateForCountry from '@salesforce/apex/InvoiceCalculator.getTaxRateForCountry';
-
-// export default class CalculateTax extends LightningElement {
-//     @track selectedCountry = '';
-//     @track calculatedTax = 0;
-//     @track countryOptions = [];
-
-//     // Load country options from the server
-//     @wire(getCountryOptions)
-//     wiredCountryOptions({ data, error }) {
-//         if (data) {
-//             this.countryOptions = data;
-//         } else if (error) {
-//             // Handle error
-//             return 0;
-//         }
-//     }
-
-//     // Handle country selection
-//     handleCountryChange(event) {
-//         this.selectedCountry = event.detail.value;
-//         this.calculateTax();
-//     }
-
-//     // Calculate tax based on the selected country
-//     calculateTax() {
-        
-//     }
-// }
